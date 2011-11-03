a0d307e383177ca719555124ba2e720ace12564f
pabo <pabo@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-11-03 11:48:05 +0000

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.10dev'
+version = '1.9multilanguagefields'
 
 long_description = (
     read('README.txt')
